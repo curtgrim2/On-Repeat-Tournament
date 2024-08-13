148523e2fb65bd298750bef042458620463d502c
curtgrim2 <59848823+curtgrim2@users.noreply.github.com>
2024-08-12 21:24:16 -0400

    Switching up the logic

--- DebateWebsite/static/js/script2.js
+++ DebateWebsite/static/js/script2.js
@@ -78,44 +78,11 @@ var testusernum = 3;
             console.log(allusers2);
            
             for(x=8; x>=1;x=x/2){ //6 rounds in Round of 64  // for(x=0;x<6;x+
-
                 console.log("Round of " + x);
-                
-                
-                for(y=0; y<numofsongsleft;y+=2){
-                   
-
-                    allusers2 = allusers2.filter(arr => arr.length > 0);
-                    console.log(allusers2);
-                if (allusers2.length < 2) {
-                    console.log("Not enough people left to continue the tournament.");
-                    break;
-                }
-
-            var randomarr = Math.floor(Math.random()*allusers2.length);
-            var randomarr2 = Math.floor(Math.random()*allusers2.length);
-
-            while(randomarr2 == randomarr ){ //Makes sure the same user isn't going against themselves //&& randomarr!=-1 && randomarr2 !=-1
-                randomarr = Math.floor(Math.random()*allusers2.length);
-                randomarr2 = Math.floor(Math.random()*allusers2.length);
-                }
-
-                
-                console.log(allusers2[randomarr][0] + " vs " + allusers2[randomarr2][0]);
-                
-                
-                
-                var eliminated = allusers2[randomarr2].shift(); // Remove the first element
-                winners.push(allusers2[randomarr][0]);
-                console.log("Eliminated: " + eliminated + ", Winner: " +  allusers2[randomarr][0]);
-
-
-
-            }
+                console.log("Number of songs remaining is " + numofsongsleft);
+                thelogic(numofsongsleft);             
                 numofsongsleft=numofsongsleft/2;
-
         }
-       
         console.log("Tournament Over");
         console.log(winners);
 
@@ -124,40 +91,85 @@ var testusernum = 3;
 
 
 
+        async function thelogic(numofsongsleft){
+            for(y=0; y<numofsongsleft;y+=2){
+                   
+                allusers2 = allusers2.filter(arr => arr.length > 0);
+                console.log(allusers2);
+            if (allusers2.length < 2) {
+                console.log("Not enough people left to continue the tournament.");
+                break;
+            }
 
+        var randomarr = Math.floor(Math.random()*allusers2.length);
+        var randomarr2 = Math.floor(Math.random()*allusers2.length);
 
+        while(randomarr2 == randomarr ){ //Makes sure the same user isn't going against themselves //&& randomarr!=-1 && randomarr2 !=-1
+            randomarr = Math.floor(Math.random()*allusers2.length);
+            randomarr2 = Math.floor(Math.random()*allusers2.length);
+            }
 
+            
+            console.log(allusers2[randomarr][0] + " vs " + allusers2[randomarr2][0]);
+            
+            var leftvidID=createvideoID(allusers2[randomarr][0]);
+            var rightvidID=createvideoID(allusers2[randomarr2][0]);
+           
+            document.getElementById("leftvid").src = "//www.youtube.com/embed/" + leftvidID;
+            document.getElementById("rightvid").src = "//www.youtube.com/embed/" + rightvidID;
 
+            var thechoice = await thewinneris('leftbutt','rightbutt');
 
-/*for(var theround=1; theround<5; theround++){
-    if(winners==0){
-        console.log();
-    }
-    else{
-
-    }
-    for (var x = 0; x < maxamountofsongs * maxamountofsongs; x++) {
-                // Filter out arrays that are empty
-                 allusers = allusers.filter(arr => arr.length > 0);
-                if (allusers.length < 2) {
-                    console.log("Not enough songs left to continue the tournament.");
-                    break;
+            if(thechoice == "Left"){
+                var eliminated = allusers2[randomarr2].shift(); // Remove the first element
+            winners.push(allusers2[randomarr][0]);
+            console.log("Eliminated: " + eliminated + ", Winner: " +  allusers2[randomarr][0]);
+            }
+            else{
+                var eliminated = allusers2[randomarr].shift(); // Remove the first element
+            winners.push(allusers2[randomarr2][0]);
+            console.log("Eliminated: " + eliminated + ", Winner: " +  allusers2[randomarr2][0]);
                 }
-                var randomuser = Math.floor(Math.random() * allusers.length);
-                var randomuser2;
-                do {
-                    randomuser2 = Math.floor(Math.random() * allusers.length);
-                } while (randomuser2 === randomuser);
-
-                console.log(allusers[randomuser][0] + " vs " + allusers[randomuser2][0]);
-
-                // The second one loses here
-                var eliminated = allusers[randomuser2].shift(); // Remove the first element
-               winners.push(allusers[randomuser][0]);
-                console.log("Eliminated: " + eliminated);
-                counter++;         
+            }
         }
-}
-        console.log("Tournament Over");
-        console.log(winners);
-*/
+
+        function thewinneris(leftop,rightop){
+            
+            return new Promise(resolve=>{
+                var left_op = document.getElementById(leftop);
+                var right_op = document.getElementById(rightop);
+
+
+                if(left_op && right_op){
+                    left_op.addEventListener('click',returnAnswer);
+                right_op.addEventListener('click',returnAnswer);
+                }
+                
+
+                function returnAnswer(event){
+                    console.log("XXXXXXXX");
+                    if(event.target.id==leftop){
+                        resolve('Left');
+                    }
+                    else{
+                        resolve('Right');
+                    }
+                    left_op.removeEventListener('click',returnAnswer);
+                    right_op.removeEventListener('click',returnAnswer);
+                }
+                
+            });
+        }
+
+
+
+        function createvideoID(theurl){
+            
+                const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+                const match = theurl.match(regExp);
+            
+                return (match && match[2].length === 11)
+                  ? match[2]
+                  : null;
+            
+        }
