086a679916c859903f3bae879e929a94d8bdef2a
curtgrim2 <59848823+curtgrim2@users.noreply.github.com>
2024-11-16 13:49:52 -0500

    Draft Save option ADDED
    Need to add the ability to pull up old drafts now (Put it in first page)

--- DebateWebsite/router.py
+++ DebateWebsite/router.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 app.secret_key = 'supersecretkey'  # Required for session management
 app.config['SESSION_TYPE'] = 'filesystem'  # Store session data on the server's filesystem
 
+dbsetup = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};" 
+                         "Server=LAPTIZZY\SQLEXPRESS;" 
+                         "Database=o_r_tournament;"
+                         "Trusted_Connection=yes;") #Format must be exactly like this, down to the spacing and new lines
+
+
+cursor = dbsetup.cursor()
+#cursor.execute('')
 
 @app.route("/")
 def home():
@@ -18,33 +26,64 @@ def home():
 @app.route('/startgame',methods=['POST','GET'])
 
 
+
 def startgame():
-    if request.method == "POST":
-        personnum=0
-        allthenames =[]
-        allurls = []
-        while f'name{personnum}' in request.form: #name{personnum} are the users unique identifiers 
+    personnum=0
+    allthenames =[]
+    allurls = []
+    
+    while f'name{personnum}' in request.form: #name{personnum} are the users unique identifiers 
             allthenames.append(request.form[f'name{personnum}'])      #We're getting the variables in the form withrequest.form 
             personnum += 1       
-        print(allthenames)
-        totalsongs = int(request.form.get("songspereach")) * int(request.form.get("numofusers"))
+        #print(allthenames)
+            totalsongs = int(request.form.get("songspereach")) * int(request.form.get("numofusers"))
 
-        print(totalsongs)
-        for x in range(totalsongs):
-            allurls.append(request.form.get(f'namenum{x}')) #Pushing each song URL from 
-            print(request.form.get(f'namenum{x}')) 
-            x+=1
+        #print(totalsongs)
+            for x in range(totalsongs):
+                allurls.append(request.form.get(f'namenum{x}')) #Pushing each song URL from 
+                #print(request.form.get(f'namenum{x}')) 
+                x+=1
+    
+    if request.form.get("checkdraftbut")=="notclicked":#if request.method == "POST":            
+        print(request.form.get("checkdraftbut"))
         return render_template('gametime.html',totalusers = int(request.form.get("numofusers")),songspereach=request.form.get("songspereach"),allnames=allthenames,utubeurls=allurls)
+    else:
+        iterthrsongs=0
+        print("Draft save button Clicked")
+        print(allurls)
+        print(allthenames)
+        print(totalsongs)
+        for x in range(len(allthenames)):
+            songsperuser =totalsongs//len(allthenames) # Double /:To prevent float TypeError
+            #print(songsperuser)
+            for y in range(songsperuser):
+                cursor.execute("""INSERT INTO saveddrafts(Name,SongURL,Notes,EntryNum)VALUES(?, ?, ?, ?)""",(allthenames[x],allurls[iterthrsongs],"",int(iterthrsongs+1)))
+                #print("Name:" + allthenames[x] + "; Song URL:" + allurls[iterthrsongs] + " - " + str(iterthrsongs))
+                iterthrsongs+=1
+                
+        cursor.commit()
+        cursor.close()
+
+        return 'DRAFT SAVED'
+        
+            
+            
+        
+        
+        
+        
  
 @app.route('/showresults',methods=['GET','POST'])       
 def showresults():
     print("Going to results page")
-    print(request.form.get('winnertiers'))
+    #print(request.form.get('winnertiers'))
+    print(request.form.get('winnernames'))
+    
     eachtier = request.form.get('winnertiers')
     
     '''for x in request.form.get('winnertiers'):
         print(x)'''
-    return render_template('resultspage.html',listoftiers=eachtier)   
+    return render_template('resultspage.html',listoftiers=eachtier,namesintiers=request.form.get('winnernames'))   
 
 
 @app.route('/savethisdraft',methods=['GET','POST'])
