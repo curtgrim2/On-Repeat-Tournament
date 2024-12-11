e60af0fabd330d66044cbd66b6aa0f7ceceb40e5
curtgrim2 <curtisgrimsley18@gmail.com>
2024-12-11 14:51:09 -0500

    Added makeshift splash screen

--- static/js/script.js
+++ static/js/script.js
@@ -219,7 +219,7 @@ function inputdraft(){
 
         //console.log(`namenum${eacheverysong++}`);
         document.getElementById(`namenumid${x}`).appendChild(utubeURLs);
-        console.log(document.getElementById(`namenum${eacheverysong++}`));
+        console.log(document.getElementById(`namenum${eeee++}`));
                             
        }
     } 
