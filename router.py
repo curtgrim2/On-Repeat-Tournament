97ba811aed139c393f70a0c066a3c5b5405e9bca
curtgrim2 <59848823+curtgrim2@users.noreply.github.com>
2026-06-09 12:06:04 -0400

    Fixed syntax errors
    Corrected environment variable name for database username and updated connection string formatting.

--- router.py
+++ router.py
@@ -17,14 +17,14 @@ app.config['SESSION_TYPE'] = 'filesystem'  # Store session data on the server's 
 #Goes to local settings for variables (just in case)
 server = os.getenv("DB_SERVER", r"localhost\SQLEXPRESS2")
 database = os.getenv("DB_NAME", "o_r_tournament")
-username = os.getenv("DB-USER")
+username = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 
 if username and password:
   #Docker uses the following if condition
   dbsetup = pyodbc.connect(f'"Driver={{ODBC Driver 17 for SQL Server}};" 
-                         f'"Server={server}" 
-                         f'"Database={database}"
+                         f"Server={server};" 
+                         f"Database={database};"
                          f"Uid={username};"
                          f"Pwd={password};"
                          "Encrypt=yes;"
@@ -33,10 +33,10 @@ if username and password:
 else:
 #When I just run it on my system
       dbsetup = pyodbc.connect(
-                         f'"Driver={{ODBC Driver 17 for SQL Server}};" 
-                         f'"Server={server};"  #localhost\SQLEXPRESS2;
-                         f'"Database=o_r_tournament;"
-                         f'"Trusted_Connection=yes;") #Format must be exactly like this, down to the spacing and new lines
+                         f"Driver={{ODBC Driver 17 for SQL Server}};" 
+                         f"Server={server};"  #localhost\SQLEXPRESS2;
+                         f"Database=o_r_tournament;"
+                         f"Trusted_Connection=yes;") #Format must be exactly like this, down to the spacing and new lines
 
 # Loop through tables and execute queries
 
